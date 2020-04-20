b566a4ea47c8cc136263434647c2688eac5ade2e
Danielle Gill <dlgill@iu.edu>
2020-04-20 17:38:42 -0400

    added some mysql and mongo stuff

---
+++ models/userActions.js
@@ -0,0 +1,27 @@
+// const dbService = require('../services/db');
+
+// class UserActions {
+
+//     constructor(review) {
+//         this.imdbId = review.imdbId;
+//         this.username = user.emails[0].value;
+//         this.review = review.reviewText;
+//     }
+
+//     static async addReview(review) {
+//         console.log('--- In the addReview function ---');
+//         //db.userActions.update({"imdbID":"tt0160182"}, {$push:{"Reviews":{"Username":"JOECOOL", "review":"very COOL"}}})
+//         var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+//         dbo.collection("customers").updateOne(review.imdbId, newvalues, function(err, res) {
+//             if (err) throw err;
+//             console.log("1 document updated");
+//             db.close();
+//         });
+//         const newReview = await dbService.db.collection('userActions').updateOne;
+//         console.log('--- Finished the updateOne statement ---');
+//         console.log(JSON.stringify(review, null, 1));
+//         return review;
+//     }
+// }
+
+// module.exports = UserActions;
