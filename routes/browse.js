7662a140de370d53c0efefc7c751f7b7ebb1336b
Noah <admin@noahschepers.com>
2020-04-22 16:53:26 -0400

    almost working entries in browse

--- routes/browse.js
+++ routes/browse.js
@@ -6,12 +6,16 @@ const connection = require('../services/sql');
 
 /* GET browse page. */
 router.get('/', async function(req, res, next) {
-    //var someMovies = await browseMovies(results);
+    var someMovies = await browseMovies();
+    console.log("--- someMoveis object: " + someMovies);
     //console.log(someMovies);
-    res.render('browse', {// send someMovies to the handlebar and display?
-        message: 'What are we mining today?', 
-        clicked: 'You\'ve been warned.'
-    });
+    // res.render('browse', {// send someMovies to the handlebar and display?
+    //     message: 'What are we mining today?',
+    //     someMovies: someMovies,
+    //     movieID: someMovies[0].imdb_title_id,
+    //     movieTitle: someMovies[0].title
+    // });
+    res.status(200).send(someMovies);
     
 });
 
@@ -34,10 +38,12 @@ const searchMovie = async function(searchIn, callback) {
 
 const browseMovies = async function(callback) {
     console.log(`--- In browseMovies function ---`);
-    connection.query(`SELECT imdb_title_id, title FROM IMDbMovies ORDER BY RAND() LIMIT 5;`, function(error, results, fields){
-        if (error) throw error;
-        callback(results);
-    });
+    connection.query(`SELECT imdb_title_id, title FROM IMDbMovies ORDER BY RAND() LIMIT 5;`,
+        function(error, results, fields){
+            if (error) throw error;
+            callback(results);
+        }
+    );
 }
 
 module.exports = router;
